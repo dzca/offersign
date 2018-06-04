827699ba096550174cf68a2095e90debfa70f846
vollov.ca <dike.zhang@gmail.com>
2018-06-03 21:39:40 -0400

    remove unused codes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,37 @@
 <template>
 <div>
-	<div class="banner-image">
+  <div class="banner-image">
+    <div class="centered-image-text">
+      <h2 class="font-weight-bold bg-text-inner">Electronic signatures<br> <small class="text-muted font-weight-bold"> for online legally binding</small></h2>
+    </div>
+  </div>
 
-		<div class="centered-image-text">
-			<h2 class="font-weight-bold bg-text-inner">Electronic signatures<br> <small class="text-muted font-weight-bold"> for online legally binding</small></h2>
-		</div>
+  <div class="container contents">
+    <div class="row">
+      <div class="col-lg-4 feature-item">
+        <img src="@/assets/imgs/easy.svg" alt="Easy image" width="140" height="140">
 
-	</div>
+        <h2 class="title">Easy</h2>
+        <p class="content">Use any device to sign any document: signing with offersign is simple, quick and secure.</p>
+      </div>
+      <!-- /.col-lg-4 -->
+      <div class="col-lg-4 feature-item">
+        <img src="@/assets/imgs/secure.svg" alt="Easy image" width="140" height="140">
 
+        <h2 class="title">Secure</h2>
+        <p class="content">Signed Document is encrypted using 256-bit SSL and handled by the reliable Amazon infrastructure.</p>
+      </div>
+      <!-- /.col-lg-4 -->
+      <div class="col-lg-4 feature-item">
+        <img src="@/assets/imgs/efficient.svg" alt="Easy image" width="140" height="140">
 
-		    <div class="container contents">
-				<div class="row">
-          <div class="col-lg-4 feature-item">
-            <img src="@/assets/imgs/easy.svg" alt="Easy image" width="140" height="140">
-
-            <h2 class="title">Easy</h2>
-            <p class="content">Use any device to sign any document: signing with offersign is simple, quick and secure.</p>
-          </div><!-- /.col-lg-4 -->
-					<div class="col-lg-4 feature-item">
-						<img src="@/assets/imgs/secure.svg" alt="Easy image" width="140" height="140">
-
-						<h2 class="title">Secure</h2>
-						<p class="content">Signed Document is encrypted using 256-bit SSL and handled by the reliable Amazon infrastructure.</p>
-					</div><!-- /.col-lg-4 -->
-					<div class="col-lg-4 feature-item">
-						<img src="@/assets/imgs/efficient.svg" alt="Easy image" width="140" height="140">
-
-						<h2 class="title">Efficient</h2>
-						<p class="content">Load your signatures automatically and optimize your document process.</p>
-					</div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-</div>
+        <h2 class="title">Efficient</h2>
+        <p class="content">Load your signatures automatically and optimize your document process.</p>
+      </div>
+      <!-- /.col-lg-4 -->
+    </div>
+    <!-- /.row -->
+  </div>
 </div>
 </template>
 
@@ -41,71 +42,47 @@ export default {
       title: "Home"
     };
   },
-  methods: {
-    nav: function(path) {
-      console.log("loading Substract Page," + path);
-      this.$router.push({ path: "/settings/" + path });
-      //this.$router.push({ name: "/settings", params: { path } });
-    }
-
-    // nav(path) {
-    //   console.log("loading Substract Page");
-    //   //this.$router.push({ name: "settings", params: { path } });
-    //   this.$router.push({ path: "/settings/${path}" });
-    // }
-  }
+  methods: {}
 };
 </script>
 <style lang="scss" scoped>
-.group {
-  border-radius: 4px;
-  min-height: 36px;
-  padding-bottom: 20px;
-}
-
 .feature-item {
-  text-align: center;
-  .title {
-    margin-top: 20px;
-    font-weight: bold;
-  }
-  .content {
-    margin: 0px 15px;
-  }
+    text-align: center;
+    .title {
+        margin-top: 20px;
+        font-weight: bold;
+    }
+    .content {
+        margin: 0 15px;
+    }
 }
 
 .banner-image {
-  background: #000 url(../assets/imgs/computer-bg-2000x630.png) no-repeat center
-    center;
-  text-align: center;
+    background: #000 url("../assets/imgs/computer-bg-2000x630.png") no-repeat center center;
+    text-align: center;
 
-  .centered-image-text {
-    position: relative;
-    // height: 460px;
-    padding-top: 10%;
-    padding-bottom: 10%;
-  }
+    .centered-image-text {
+        position: relative;
+        padding-top: 10%;
+        padding-bottom: 10%;
+    }
 }
 
 .image {
-  // width: 100%;
-  // display: block;
-
-  height: 70px;
-  width: 70px;
-  margin: 10% auto 7%;
+    height: 70px;
+    width: 70px;
+    margin: 10% auto 7%;
 }
 
 .el-card {
-  // border: 1px solid #ebeef5;
-  border: #fff;
-  text-align: center;
-  font-weight: bold;
+    border: #fff;
+    text-align: center;
+    font-weight: bold;
 
-  .bottom {
-    font-weight: normal;
-    margin-top: 13px;
-    line-height: 20px;
-  }
+    .bottom {
+        font-weight: normal;
+        margin-top: 13px;
+        line-height: 20px;
+    }
 }
 </style>
--- src/pages/Pricing.vue
+++ src/pages/Pricing.vue
@@ -1,60 +1,58 @@
 <template>
 <div>
-	<div class="banner-image">
-		<div class="centered-image-text">Centered</div>
-	</div>
+  <div class="banner-image">
+    <div class="centered-image-text">Centered</div>
+  </div>
 
-
-
-		    <div class="container contents">
-		      <div class="card-deck mb-3 text-center">
-		        <div class="card mb-4 box-shadow">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Free</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>10 users included</li>
-		              <li>2 GB of storage</li>
-		              <li>Email support</li>
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-		          </div>
-		        </div>
-		        <div class="card mb-4 box-shadow">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Pro</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>20 users included</li>
-		              <li>10 GB of storage</li>
-		              <li>Priority email support</li>
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-		          </div>
-		        </div>
-		        <div class="card mb-4 box-shadow">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>30 users included</li>
-		              <li>15 GB of storage</li>
-		              <li>Phone and email support</li>ad-image
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-		          </div>
-		        </div>
-		      </div>
-					</div>
+  <div class="container contents">
+    <div class="card-deck mb-3 text-center">
+      <div class="card mb-4 box-shadow">
+        <div class="card-header">
+          <h4 class="my-0 font-weight-normal">Free</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>10 users included</li>
+            <li>2 GB of storage</li>
+            <li>Email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+        </div>
+      </div>
+      <div class="card mb-4 box-shadow">
+        <div class="card-header">
+          <h4 class="my-0 font-weight-normal">Pro</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>20 users included</li>
+            <li>10 GB of storage</li>
+            <li>Priority email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+        </div>
+      </div>
+      <div class="card mb-4 box-shadow">
+        <div class="card-header">
+          <h4 class="my-0 font-weight-normal">Enterprise</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>30 users included</li>
+            <li>15 GB of storage</li>
+            <li>Phone and email support</li>ad-image
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -65,34 +63,21 @@ export default {
       title: "About"
     };
   },
-  methods: {
-    nav: function(path) {
-      console.log("loading Substract Page," + path);
-      this.$router.push({ path: "/settings/" + path });
-      //this.$router.push({ name: "/settings", params: { path } });
-    }
-
-    // nav(path) {
-    //   console.log("loading Substract Page");
-    //   //this.$router.push({ name: "settings", params: { path } });
-    //   this.$router.push({ path: "/settings/${path}" });
-    // }
-  }
+  methods: {}
 };
 </script>
 <style lang="scss" scoped>
 .banner-image {
-  background: #000 url(../assets/imgs/pad-bg-2000x630.png) no-repeat 0;
-  height: 460px;
-  // margin-top: 90px;
-  position: relative;
-  width: 100% !important;
+    background: #000 url("../assets/imgs/pad-bg-2000x630.png") no-repeat 0;
+    height: 460px;
+    position: relative;
+    width: 100% !important;
 
-  .centered-image-text {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
+    .centered-image-text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 }
 </style>
